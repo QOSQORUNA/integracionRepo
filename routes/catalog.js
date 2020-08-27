b0645be1afcf5b5a9bc84c95a31c96cc25d9362d
qosqoruna1996 <jose.lovaton@unas.edu.pe>
2020-08-27 16:23:55 -0500

    update class

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const catalog=[
+  {id:1,name:'Samsung j1', price: 400.00},
+  {id:2,name:'Nokia asha', price: 100.00},
+  {id:3,name:'Samsung j2', price: 500.00},
+  {id:4,name:'Samsung j1', price: 400.00},
+  {id:5,name:'Samsung j1', price: 400.00}
+];
 
 /* GET users listing. */
 router.get('/', function(request, response) {
   //response.send('catalog');
-  const catalog=[
-    {id:1,name:'Samsung j1', price: 400.00},
-    {id:1,name:'Nokia asha', price: 100.00},
-    {id:1,name:'Samsung j2', price: 500.00},
-    {id:1,name:'Samsung j1', price: 400.00},
-    {id:1,name:'Samsung j1', price: 400.00}
-  ];
-  response.send(catalog);
+  
+  response.json(catalog);
+});
+
+// router.get('/:product_id', function(request, response){
+//   const product = request.params.product_id;
+//   var product = catalog.find(item => item.id == product_id); 
+//   response.json(product);
+// });
+
+
+router.get('/:productId', function(request, response){
+  const productId = request.params.productId;
+  var product = catalog.find(item => item.id == productId); 
+
+  response.json(product);
 });
 
 module.exports = router;
